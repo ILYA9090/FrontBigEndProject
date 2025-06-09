f0ffbbdfe3d2ca48c87fc343a82b61ccfaedb024
Ilya Ovchinnikov <ilya1632@yandex.ru>
2025-06-09 19:31:23 +0400

    add context for global theme and create custom hook for change and set Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Suspense } from 'react'
+import { Suspense, useContext, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-
-import './index.scss'
-// import MainPage from './pages/MainPage/MainPage'
-// import AboutPage from './pages/AboutPage/AboutPage'
+import './styles/index.scss';
 import { Link } from 'react-router-dom'
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import useTheme from './theme/useTheme';
+
 export default function App() {
+  const {theme, toggleTheme} = useTheme()
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to='/'>главная</Link>
       <Link to='/about'>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
