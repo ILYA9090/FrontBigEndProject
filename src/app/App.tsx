4c13a1499efa9688e25ea57500198a573ee63457
Ilya Ovchinnikov <ilya1632@yandex.ru>
2025-06-13 22:52:09 +0400

    сделать компонент AppLink переиспользуемым и сделать его как UI KIT, добавить стилизацию, создать снипет для развертывания компонента

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,16 @@
-import { Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
 import './styles/index.scss';
 import { Link } from 'react-router-dom'
-import AboutPage from 'pages/AboutPage/ui/AboutPage';
-import MainPage from 'pages/MainPage/ui/MainPage';
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
-
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/navbar';
 export default function App() {
   const {theme, toggleTheme} = useTheme()
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to='/'>главная</Link>
-      <Link to='/about'>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/about' element={<AboutPage/>}/>
-          <Route path='/' element={<MainPage/>}/>
-        </Routes>
-      </Suspense>
-       
+      <Navbar/>
+      <AppRouter/>
+       <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   )
 }
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,10 +4,10 @@
 //Record это обьект с ограниченным количеством значений, в данном случае у нас ключ это стринг а значение это строка или булеан.
 type Mods = Record<string, string| boolean>
 
-export function classNames (cls: string, mods: Mods, additional: string[]): string {
+export function classNames (cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods). 
         filter(([className, value]) => Boolean(value)). 
         map(([className]) => className)
