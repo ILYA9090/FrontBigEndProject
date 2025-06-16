cb3bfd630c03e76908f31a166b87afebfcf5837a
Ilya Ovchinnikov <ilya1632@yandex.ru>
2025-06-16 21:51:04 +0400

    setting i18n and add translate for lang

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,27 @@
 import './styles/index.scss';
+import { Suspense } from 'react';
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/navbar';
 import { Sidebar } from 'widgets/Sidebar';
+
+
+
+
 export default function App() {
-  const {theme } = useTheme()
+
+  const {theme} = useTheme()
+
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar/>
-      <div className='content-page'>
-        <Sidebar/>
-        <AppRouter/>
-      </div>      
+      <Suspense fallback="">
+        <Navbar/> 
+          <div className='content-page'>
+            <Sidebar/>
+            <AppRouter/>
+          </div>      
+      </Suspense>     
     </div>
   )
 }
