dc319acd4105e45f1451414cc7c55926fb8cd2ff
Ilya Ovchinnikov <ilya1632@yandex.ru>
2025-06-24 14:38:03 +0400

    test ci

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -20,7 +20,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
   return (
     <div data-testid="sidebar" className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
       <Button data-testid="sidebar-toggle" onClick={onToggle}>
-        {t('toggle')}
+        {t('переключить')}
       </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
