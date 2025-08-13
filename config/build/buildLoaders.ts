42dcdf9527d2d93f09c20aec1acc5fb7c7fddf40
Ilya Ovchinnikov <ilya1632@yandex.ru>
2025-08-13 15:04:52 +0400

    миграция на babelLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,14 +10,16 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
   };
-  const bableLoader = buildBabelLoader(options);
+  const codeBableLoader = buildBabelLoader({ ...options, isTsx: false });
+  const tsxCodeBableLoader = buildBabelLoader({ ...options, isTsx: true });
+
   const cssLoaders = buildCssLoader(isDev);
 
-  const typeScriptLoader = {
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
-  };
+  // const typeScriptLoader = {
+  //   test: /\.tsx?$/,
+  //   use: 'ts-loader',
+  //   exclude: /node_modules/,
+  // };
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif)$/i,
@@ -28,5 +30,11 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [fileLoader, svgLoaders, bableLoader, typeScriptLoader, cssLoaders];
+  return [
+    fileLoader,
+    svgLoaders,
+    codeBableLoader,
+    tsxCodeBableLoader,
+    cssLoaders,
+  ];
 }
